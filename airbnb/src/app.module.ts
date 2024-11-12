582a674ef94fce4142c79d39a63e07991d5755e8
KaiPow1206 <nguyennhoanhthai@gmail.com>
2024-11-12 14:45:51 +0700

    done set up

--- airbnb/src/app.module.ts
+++ airbnb/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BinhLuanModule } from './binh-luan/binh-luan.module';
+import { DatPhongModule } from './dat-phong/dat-phong.module';
+import { NguoiDungModule } from './nguoi-dung/nguoi-dung.module';
+import { PhongModule } from './phong/phong.module';
+import { ViTriModule } from './vi-tri/vi-tri.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, BinhLuanModule, DatPhongModule, NguoiDungModule, PhongModule, ViTriModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ airbnb/src/vi-tri/dto/update-vi-tri.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateViTriDto } from './create-vi-tri.dto';
+
+export class UpdateViTriDto extends PartialType(CreateViTriDto) {}
