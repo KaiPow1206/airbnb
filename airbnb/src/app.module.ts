3c09b5323d1cde187cd053e6711c25e5276ec670
KaiPow1206 <nguyennhoanhthai@gmail.com>
2024-11-12 15:47:09 +0700

    new

--- airbnb/src/app.module.ts
+++ airbnb/src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { BinhLuanModule } from './binh-luan/binh-luan.module';
 import { DatPhongModule } from './dat-phong/dat-phong.module';
 import { NguoiDungModule } from './nguoi-dung/nguoi-dung.module';
 import { PhongModule } from './phong/phong.module';
 import { ViTriModule } from './vi-tri/vi-tri.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, BinhLuanModule, DatPhongModule, NguoiDungModule, PhongModule, ViTriModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    AuthModule,
+    BinhLuanModule, 
+    DatPhongModule, 
+    NguoiDungModule, 
+    PhongModule, 
+    ViTriModule],
   controllers: [AppController],
   providers: [AppService],
 })
